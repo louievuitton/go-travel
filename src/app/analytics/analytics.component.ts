135ca281353c1ec76a0de260055c30f71f460aec
Charles <charlespatel07@gmail.com>
2020-04-17 12:14:41 -0700

    restaurant done

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -13,7 +13,9 @@ export class AnalyticsComponent implements OnInit {
   articles;
   placeDetails;
   venues:string[] = new Array();
+  photos:string[] = new Array();
   placePhoto;
+  url;
 
   public card = new Card({
         imageUrl: "https://www.infragistics.com/angular-demos/assets/images/card/media/ny.jpg",
@@ -22,10 +24,17 @@ export class AnalyticsComponent implements OnInit {
 
   ngOnInit() {
     this.apiService.getNews().subscribe((data)=>{
-      this.articles = data['response'].groups[0].items;
+      console.log(data);
+      this.articles = data['results'];
       for (let num of this.articles) {
-        this.venues.push(num.venue.id);
+        this.venues.push(num.place_id);
+        this.apiService.getStats(num.place_id).subscribe((data)=>{
+        this.placeDetails = data['result'];
+        this.placePhoto = this.placeDetails.photos;
+        this.photos.push(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=200&photoreference=${this.placePhoto[0].photo_reference}&key=AIzaSyDSV9VwCxhAK_m0n1qFr2KpJxLpFvhVDfM`);
+      });
       }
+      console.log(this.venues);
     });
   }
 
